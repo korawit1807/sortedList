76c2c45f78012a89734926f363fb2a615e5e4768
korawit1807 <korawit.sj@gmail.com>
2020-05-17 17:49:54 +0700

    ADD

---
+++ SORTEDLIST_HOMEWORK/SORTEDLIST_HOMEWORK/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SORTEDLIST_HOMEWORK
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SortedList sortedList = new SortedList();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void addBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string dataShow = "";
+            if (txtKey.Text == "" || txtValue.Text == "") 
+            {
+                MessageBox.Show("กรุณากรอกข้อมูล");
+            }
+            else {
+                sortedList.Add(txtKey.Text, txtValue.Text);
+                ICollection collection = sortedList.Keys;
+                foreach (string sh in collection)
+                {
+                    dataShow = dataShow + sortedList[sh].ToString()+"\n";
+                }
+                MessageBox.Show(dataShow);
+            }
+        }
+    }
+}
